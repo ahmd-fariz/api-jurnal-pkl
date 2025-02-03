24df317731c2f2ab0e274b71a7851907f9667fbe
ahmd-fariz <ahmadfarizmaulana4@gmail.com>
2025-02-03 13:06:57 +0700

    route siswa

--- app/routes/siswa.js
+++ app/routes/siswa.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
 
   router.get("/", siswa.findAll);
   router.post("/", upl_fotoSiswa.single("siswa_foto"), siswa.create);
+  router.patch("/:id", upl_fotoSiswa.single("siswa_foto"), siswa.update);
+  router.delete("/", siswa.delete);
 
   app.use("/api/siswa", router);
 };
